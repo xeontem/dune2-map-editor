fb946f276fc2f18634b0758494805d622017903f
ARTSIOM KUKHAREV <xeontem@gmail.com>
2021-01-17 03:00:07 -0800

    feat: render terrain

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-var mapGrid = [
-    ["DC", "AF", "12", "3E", "3E"],
-    ["DC", "AF", "12", "32", "32"],
-    ["DC", "AF", "12", "32", "32"],
-    ["DC", "AF", "12", "E3", "E3"],
-    ["DC", "AF", "12", "33", "33"]
-];
-
-var mapDiv = document.createElement('DIV');
-mapDiv.classList.add('map');
-document.body.appendChild(mapDiv);
-
-mapGrid.forEach(row => {
-    var rowDiv = document.createElement('DIV');
-    rowDiv.classList.add('row');
-
-    row.forEach(spriteName => {
-        var spriteDiv = document.createElement('DIV');
-        spriteDiv.innerHTML = spriteName;
-        rowDiv.appendChild(spriteDiv);
+function renderTerrain(matrix) {
+    var mapDiv = document.createElement('DIV');
+    mapDiv.classList.add('map');
+    document.body.appendChild(mapDiv);
+    
+    matrix.forEach(row => {
+        var rowDiv = document.createElement('DIV');
+        rowDiv.classList.add('row');
+    
+        row.forEach(spriteName => {
+            var imgEl = document.createElement('IMG');
+            imgEl.src = "terrain/" + spriteName + ".png";
+            rowDiv.appendChild(imgEl);
+        });
+    
+        mapDiv.appendChild(rowDiv);
     });
+}
 
-    mapDiv.appendChild(rowDiv);
-});
+function parseTerrain(rawTerrain, width) {
+    var parsedTerrainMatrix = [];
+
+    var currentRow = [];
+    var x = 0;
+    
+    rawTerrain.forEach(spriteName => {
+        x++;
+        currentRow.push(spriteName);
+        if (x === (width - 1)) {
+            parsedTerrainMatrix.push(currentRow);
+        }
+    
+        if (x == width) {
+            x = 0;
+            currentRow = [];
+        }
+    });
+    return parsedTerrainMatrix;
+}
+
+renderTerrain(parseTerrain(map0.terrainMatrix, map0.terrainWidth));
